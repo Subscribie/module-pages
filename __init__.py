7747446b456e3f79f2c6bbc6291be4ece613d8b6
chrisjsimpson <chris@karmacomputing.co.uk>
2020-02-18 23:04:36 +0000

    wip added pages module index template TODO: add/edit/delete pages. CRUD.

--- __init__.py
+++ __init__.py
@@ -3,11 +3,12 @@ from jinja2 import TemplateNotFound
 from subscribie.db import get_jamla
 from subscribie.auth import login_required
 
-module_pages = Blueprint('module_pages', __name__, template_folder='templates')
+module_pages = Blueprint('pages', __name__, template_folder='templates')
 
-@module_pages.route('/module_pages/index') # Module index page
+@module_pages.route('/pages/index') # Module index page
+@module_pages.route('/Add-pages')
 @login_required
 def get_module_pages_index():
     """Return module_pages index page"""
 
-    return "TODO"
+    return render_template('module_pages_index.html', jamla=get_jamla())
