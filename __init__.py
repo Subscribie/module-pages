339af446fdf2b5c8b671a2e82d2563f3ce124cd7
chrisjsimpson <chris@karmacomputing.co.uk>
2020-02-19 22:48:41 +0000

    WIP adding new page https://github.com/Subscribie/subscribie/issues/121

--- __init__.py
+++ __init__.py
@@ -12,3 +12,23 @@ def get_module_pages_index():
     """Return module_pages index page"""
 
     return render_template('module_pages_index.html', jamla=get_jamla())
+
+@module_pages.route('/add-page')
+@login_required
+def add_page():
+    """Return add page form"""
+    return render_template('add_page.html', jamla=get_jamla())
+
+@module_pages.route('/add-page', methods=['POST'])
+@login_required
+def save_new_page():
+    try:
+        page_title = request.form['page-title']
+    except KeyError:
+        return "Error: Page title is required"
+
+    try:
+        page_body = request.form['page-body']
+    except KeyError:
+        return "Error: Page body is required"
+    return page_title + page_body
