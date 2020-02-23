bf1090f45489072cf5a9b40ffa01501ca70cf303
chrisjsimpson <chris@karmacomputing.co.uk>
2020-02-23 14:19:45 +0000

    wip edit existing pages

--- __init__.py
+++ __init__.py
@@ -22,6 +22,76 @@ def add_page():
     """Return add page form"""
     return render_template('add_page.html', jamla=get_jamla())
 
+@module_pages.route('/edit-pages')
+@login_required
+def edit_pages_list():
+    jamla = get_jamla()
+    pages = jamla['pages']
+    return render_template('edit_pages_list.html', pages=pages, jamla=get_jamla())
+
+@module_pages.route('/edit-page/<path>', methods=['POST', 'GET'])
+@login_required
+def edit_page(path):
+    """Edit a given page"""
+    jamla = get_jamla()
+    if request.method == 'GET':
+        # Get page file contents
+        for pageName in jamla['pages']:
+            try:
+                template_file = pageName[path]['template_file']
+            except KeyError:
+                pass
+        with open(Path(str(current_app.config['THEME_PATH']), template_file)) as fh:
+            rawPageContent = fh.read()
+        return render_template('edit_page.html', jamla=jamla, rawPageContent=rawPageContent, pageTitle=path)
+
+    elif request.method == 'POST':
+        try:
+            page_title = request.form['page-title']
+        except KeyError:
+            return "Error: Page title is required"
+
+        try:
+            page_body = request.form['page-body']
+        except KeyError:
+            return "Error: Page body is required"
+
+        # Generate a valid path for url
+        pageName = ''
+        for char in page_title:
+            if char.isalnum():
+                pageName += char
+        # Generate a valid html filename
+        template_file = pageName + '.html'
+
+        # Writeout template_file to file
+        with open(Path(str(current_app.config['THEME_PATH']), template_file), 'w') as fh:
+            fh.write(page_body)
+
+        # Add path to jamla and save jamla
+        pathDict = {pageName: {'path': pageName, 'template_file' : template_file}}
+        jamla = get_jamla()
+        # Check page doesnt already exist
+        pageExists = False
+        for page in jamla['pages']:
+            try:
+                page[pageName]
+                pageExists = True
+            except KeyError:
+                pass
+        if pageExists is False:
+            jamla['pages'].append(pathDict)
+            with open(current_app.config["JAMLA_PATH"], "w") as fh:
+                yaml.safe_dump(jamla, fh, default_flow_style=False)
+            flash(Markup('Your page <a href="/{}">{}</a> will be visable after reloading'.format(pageName, pageName)))
+        else:
+            flash(Markup('Page edited. <a href="/{}">{}</a> '.format(pageName, pageName)))
+
+        # Graceful reload app to load new page
+        return redirect(url_for('views.reload_app'))
+
+
+
 @module_pages.route('/add-page', methods=['POST'])
 @login_required
 def save_new_page():
